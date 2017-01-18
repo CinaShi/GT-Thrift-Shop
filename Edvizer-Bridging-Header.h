4b090926ed30409dfb6886e09f518e0bb6fce464
Shi Mengyang <smengyang@lenovo.com>
2017-01-17 21:08:22 -0500

    old works uploaded

--- Edvizer-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "AlertViewController.h"
-#import "SettingViewController.h"
-#import "AnalyticsHistoryViewController.h"
-
-#import "UIColor+Colors.h"
--- Podfile
+++
@@ -1,2 +0,0 @@
-pod 'SDWebImage', '~>3.7'
-pod 'AFNetworking', '~> 3.0'
