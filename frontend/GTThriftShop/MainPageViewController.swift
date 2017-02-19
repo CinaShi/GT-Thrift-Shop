77ddb9f933b35852735b4351d99076aa3d0f7221
CinaShi <ccc1nnn4@gmail.com>
2017-02-19 00:18:39 -0500

    Tag function done. Sprint 2 all functions done.

--- frontend/GTThriftShop/MainPageViewController.swift
+++ frontend/GTThriftShop/MainPageViewController.swift
@@ -13,7 +13,10 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
     var selected: Product?
     var userDefaults = UserDefaults.standard
     var searchActive: Bool = false
+    var menuShowing = false
     
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     var filteredProducts = [Product]()
     
     @IBOutlet weak var searchBar: UISearchBar!
@@ -22,26 +25,42 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var timeSorting: UIButton!
     @IBOutlet weak var priceSorting: UIButton!
     
+    @IBOutlet weak var menuTableView: UITableView!
+    var tags = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Do any additional setup after loading the view, typically from a nib.
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
         searchBar.delegate = self
+        
+        self.menuTableView.dataSource = self
+        self.menuTableView.delegate = self
+        
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+        self.menuView.layer.shadowOpacity = 1
+        self.menuView.layer.shadowRadius = 6
+        leadingConstraint.constant = -140
+        self.view.layoutIfNeeded()
+        tags.append("All")
+        tags.append("Calculator")
+        tags.append("Computer")
     }
     
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
         products.removeAll()
         
         obtainAllProductsFromServer()
         
         tableView.reloadData()
         
-        
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
     }
     
     //Mark: helper methods
@@ -51,6 +70,15 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
         let encodedData: Data = NSKeyedArchiver.archivedData(withRootObject: productsToSave)
         userDefaults.set(encodedData, forKey: "products")
         userDefaults.synchronize()
+    }
+    
+    func refreshProductsFromLocal() {
+        if let decoded = userDefaults.object(forKey: "products") as? Data {
+            products = NSKeyedUnarchiver.unarchiveObject(with: decoded) as! [Product]
+        } else {
+            notifyFailure(info: "Please connect to Internet")
+            //actually might need to manually grab data from server again here. Need opinions
+        }
     }
     
     func obtainAllProductsFromServer() {
@@ -101,9 +129,7 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
 
                             let newProduct = Product(name: name, price: price, info: info, pid: pid, postTime: postTime, usedTime: usedTime, userId: userId, imageUrls: imageUrls)
                             self.products.append(newProduct)
-                            let dateFormatter = DateFormatter()
-                            dateFormatter.dateFormat = "EEE, dd LLL yyyy HH:mm:ss z"
-                            self.products.sort(by: {dateFormatter.date(from: $0.postTime)! > dateFormatter.date(from: $1.postTime)!})
+                            
                         }
                     } catch let error as NSError {
                         print("Failed to load: \(error.localizedDescription)")
@@ -111,6 +137,7 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
                     
                     
                     DispatchQueue.main.async(execute: {
+                        self.initialSort()
                         self.loadProductsIndicator.stopAnimating()
                         self.storeProductsToLocal()
                         self.tableView.reloadData()
@@ -134,6 +161,86 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
         }
         
         task.resume()
+    }
+    
+    func getPidsByTag(tag: String) {
+        loadProductsIndicator.startAnimating()
+        let url = URL(string: "http://ec2-34-196-222-211.compute-1.amazonaws.com/products/tags/\(tag)")
+        
+        var request = URLRequest(url:url! as URL)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error!)")
+                DispatchQueue.main.async(execute: {
+                    self.notifyFailure(info: "There might be some connection issue. Please try again!")
+                });
+                
+                return
+            }
+            
+            // You can print out response object
+            print("******* response = \(response!)")
+            
+            // Print out reponse body
+            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print("****** response data = \(responseString!)")
+            if let httpResponse = response as? HTTPURLResponse {
+                print("***** statusCode: \(httpResponse.statusCode)")
+                if httpResponse.statusCode == 200 {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as! Dictionary<String, Any>
+                        let array = json["pids"] as! [Int]
+                        DispatchQueue.main.async(execute: {
+                            self.reloadTableWithTaggedPids(pids: array)
+                        });
+                    } catch let error as NSError {
+                        print("Failed to load: \(error.localizedDescription)")
+                    }
+                    
+                    
+                    DispatchQueue.main.async(execute: {
+                        self.loadProductsIndicator.stopAnimating()
+                    });
+                }else if httpResponse.statusCode == 404 {
+                    DispatchQueue.main.async(execute: {
+                        self.notifyFailure(info: "Cannot connect to Internet!")
+                    });
+                }
+                else {
+                    DispatchQueue.main.async(execute: {
+                        self.notifyFailure(info: "There might be some connection issue. Please try again!")
+                    });
+                    
+                }
+            } else {
+                DispatchQueue.main.async(execute: {
+                    self.notifyFailure(info: "There might be some connection issue. Please try again!")
+                });
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func reloadTableWithTaggedPids(pids: [Int]) {
+        refreshProductsFromLocal()
+        
+        products = products.filter{ item in
+            return pids.contains(item.pid!)
+        }
+        
+        initialSort()
+        self.tableView.reloadData()
+    }
+    
+    func initialSort() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE, dd LLL yyyy HH:mm:ss z"
+        self.products.sort(by: {dateFormatter.date(from: $0.postTime)! > dateFormatter.date(from: $1.postTime)!})
     }
     
     func notifyFailure(info: String) {
@@ -178,6 +285,18 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     
+    @IBAction func openMenu(_ sender: Any) {
+        leadingConstraint.constant = 0
+        UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded()})
+        menuShowing = true
+    }
+    
+    @IBAction func closeMenu(_ sender: Any) {
+        leadingConstraint.constant = -140
+        UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded()})
+        menuShowing = false
+    }
+    
     //Mark: Table view delegate
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -185,50 +304,88 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if searchActive {
-            return filteredProducts.count
+        var count:Int?
+        if tableView == self.tableView {
+            if searchActive {
+                count = filteredProducts.count
+            } else {
+                count = products.count
+            }
         }
-        return products.count
+        
+        if tableView == self.menuTableView {
+            count = tags.count
+        }
+        
+        return count!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Table view cells are reused and should be dequeued using a cell identifier.
-        let cellIdentifier = "mainPageItemCell"
-        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        var currentProduct = products[indexPath.row]
-        if searchActive {
-            currentProduct = filteredProducts[indexPath.row]
+        var cell:UITableViewCell?
+        
+        if tableView == self.tableView {
+            
+            let cellIdentifier = "mainPageItemCell"
+            cell = self.tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+            var currentProduct = products[indexPath.row]
+            if searchActive {
+                currentProduct = filteredProducts[indexPath.row]
+            }
+            
+            // Fetches the banks for the data source layout.
+            let itemImage = cell?.contentView.viewWithTag(5) as! UIImageView
+            let itemNameLabel = cell?.contentView.viewWithTag(1) as! UILabel
+            let yearUsedLabel = cell?.contentView.viewWithTag(2) as! UILabel
+            let priceLabel = cell?.contentView.viewWithTag(3) as! UILabel
+            let sellerLabel = cell?.contentView.viewWithTag(4) as! UILabel
+            
+            if let imageData: NSData = NSData(contentsOf: URL(string: currentProduct.imageUrls.first!)!) {
+                itemImage.image = UIImage(data: imageData as Data)
+            } else {
+                itemImage.image = #imageLiteral(resourceName: "calculator")
+            }
+            itemNameLabel.text = currentProduct.name
+            yearUsedLabel.text = currentProduct.usedTime
+            priceLabel.text = currentProduct.price
+            sellerLabel.text = "user ID: \(currentProduct.userId!)"
         }
-        // Fetches the banks for the data source layout.
-        let itemImage = cell.contentView.viewWithTag(5) as! UIImageView
-        let itemNameLabel = cell.contentView.viewWithTag(1) as! UILabel
-        let yearUsedLabel = cell.contentView.viewWithTag(2) as! UILabel
-        let priceLabel = cell.contentView.viewWithTag(3) as! UILabel
-        let sellerLabel = cell.contentView.viewWithTag(4) as! UILabel
         
-        if let imageData: NSData = NSData(contentsOf: URL(string: currentProduct.imageUrls.first!)!) {
-            itemImage.image = UIImage(data: imageData as Data)
-        } else {
-            itemImage.image = #imageLiteral(resourceName: "calculator")
+        if tableView == self.menuTableView {
+            let cellIdentifier = "menuCell"
+            cell = self.menuTableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+            let tagNameLabel = cell?.contentView.viewWithTag(1) as! UILabel
+            tagNameLabel.text = tags[indexPath.row]
         }
-        itemNameLabel.text = currentProduct.name
-        yearUsedLabel.text = currentProduct.usedTime
-        priceLabel.text = currentProduct.price
-        sellerLabel.text = "user ID: \(currentProduct.userId!)"
         
-        
-        return cell
+        return cell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
-        if searchActive {
-            selected = filteredProducts[indexPath.row]
-        } else {
-            selected = products[indexPath.row]
+        if tableView == self.tableView {
+            if searchActive {
+                selected = filteredProducts[indexPath.row]
+            } else {
+                selected = products[indexPath.row]
+            }
+            performSegue(withIdentifier: "getItemDetails", sender: nil)
         }
-        performSegue(withIdentifier: "getItemDetails", sender: nil)
+        
+        if tableView == self.menuTableView {
+            closeMenu(self)
+            print("should send url here")
+            if tags[indexPath.row] == "All" {
+                refreshProductsFromLocal()
+                initialSort()
+                self.tableView.reloadData()
+            } else {
+                getPidsByTag(tag: tags[indexPath.row])
+            }
+        }
     }
+    
+    //Mark:Search bar delegate
     
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchActive = true
@@ -269,6 +426,5 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
         }
         
     }
-    
     
 }
