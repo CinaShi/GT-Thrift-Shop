b369be62e813acd4074ea449d5b8d608827a234e
alexan0218 <alexan0218@gmail.com>
2017-03-02 01:23:26 -0500

    Today's last update

--- frontend/GTThriftShop/FirstTimeSuccessViewController.swift
+++ frontend/GTThriftShop/FirstTimeSuccessViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FirstTimeSuccessViewController: UIViewController {
     
+    @IBOutlet weak var background: UIImageView!
     @IBOutlet weak var submitButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +18,15 @@ class FirstTimeSuccessViewController: UIViewController {
         submitButton.layer.borderWidth = 1
         submitButton.layer.borderColor = UIColor(red: 0, green: 128/255, blue: 1, alpha: 1).cgColor
         submitButton.layer.cornerRadius = 20
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        let width = UIScreen.main.bounds.size.width
+        let height = UIScreen.main.bounds.size.height
+        
+        blurView.frame.size = CGSize(width: width, height: height)
+        blurView.alpha = 0.9
+        background.addSubview(blurView)
+
     }
     
     override func didReceiveMemoryWarning() {
