0251e9dfe666e9898fb0f91511a9a12b4829a5fc
alexan0218 <alexan0218@gmail.com>
2017-04-12 04:15:31 -0400

    merge

--- frontend/GTThriftShop/EmbedTableViewController.swift
+++ frontend/GTThriftShop/EmbedTableViewController.swift
@@ -16,11 +16,11 @@ class EmbedTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.extraLight)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        view.addSubview(blurEffectView)
+        view.sendSubview(toBack: blurEffectView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,15 +30,7 @@ class EmbedTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
+
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let tableViewCell = super.tableView(tableView, cellForRowAt: indexPath)
